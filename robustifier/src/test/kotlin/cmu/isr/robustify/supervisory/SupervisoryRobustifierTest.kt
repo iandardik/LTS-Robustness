63d4bfb61a1e658a889682d4806c305bf25ff2b1
Changjian Zhang <mljzcj@gmail.com>
2022-07-11 14:58:48 -0400

    Add therac and pump tests

--- robustifier/src/test/kotlin/cmu/isr/robustify/supervisory/SupervisoryRobustifierTest.kt
+++ robustifier/src/test/kotlin/cmu/isr/robustify/supervisory/SupervisoryRobustifierTest.kt
@@ -1,9 +1,9 @@
 package cmu.isr.robustify.supervisory
 
-import cmu.isr.lts.checkDeadlock
 import cmu.isr.ltsa.LTSACall
 import cmu.isr.ltsa.LTSACall.asDetLTS
 import cmu.isr.ltsa.LTSACall.compose
+import cmu.isr.robustify.desops.DESopsRunner
 import cmu.isr.robustify.supremica.SupremicaRunner
 import net.automatalib.words.Word
 import org.junit.jupiter.api.Test
@@ -18,21 +18,18 @@ class SupervisoryRobustifierTest {
     val envSpec =
       ClassLoader.getSystemResource("specs/voting/env2.lts")?.readText() ?: error("Cannot find voting/env2.lts")
     val pSpec = ClassLoader.getSystemResource("specs/voting/p.lts")?.readText() ?: error("Cannot find voting/p.lts")
-    val backSpec =
-      ClassLoader.getSystemResource("specs/voting/back.lts")?.readText() ?: error("Cannot find voting/back.lts")
 
     val sys = LTSACall.compile(sysSpec).compose().asDetLTS()
     val env = LTSACall.compile(envSpec).compose().asDetLTS()
     val safety = LTSACall.compile(pSpec).compose().asDetLTS()
-    val back = LTSACall.compile(backSpec).compose().asDetLTS()
-    val backTrace = checkDeadlock(back, back.inputAlphabet).trace!!
+    val back = Word.fromSymbols("select", "back")
 
     return SupervisoryRobustifier(
       sys, sys.inputAlphabet,
       env, env.inputAlphabet,
       safety, safety.inputAlphabet,
       progress = listOf("confirm"),
-      preferredMap = mapOf(Priority.P3 to listOf(backTrace)),
+      preferredMap = mapOf(Priority.P3 to listOf(back)),
       controllableMap = mapOf(
         Priority.P0 to listOf("back", "confirm", "password", "select", "vote"),
         Priority.P3 to listOf("eo.enter", "eo.exit", "v.enter", "v.exit")
@@ -40,6 +37,124 @@ class SupervisoryRobustifierTest {
       observableMap = mapOf(
         Priority.P0 to listOf("back", "confirm", "password", "select", "vote"),
         Priority.P2 to listOf("eo.enter", "eo.exit", "v.enter", "v.exit")
+      ),
+      synthesizer = SupremicaRunner(),
+      maxIter = 1
+    )
+  }
+
+  private fun loadTherac(): SupervisoryRobustifier {
+    val sysSpec =
+      ClassLoader.getSystemResource("specs/therac25/sys.lts")?.readText() ?: error("Cannot find therac25/sys.lts")
+    val envSpec =
+      ClassLoader.getSystemResource("specs/therac25/env.lts")?.readText() ?: error("Cannot find therac25/env.lts")
+    val pSpec = ClassLoader.getSystemResource("specs/therac25/p.lts")?.readText() ?: error("Cannot find therac25/p.lts")
+    val back1 = Word.fromSymbols("x", "up")
+    val back2 = Word.fromSymbols("e", "up")
+    val back3 = Word.fromSymbols("enter", "up")
+
+    val sys = LTSACall.compile(sysSpec).compose().asDetLTS()
+    val env = LTSACall.compile(envSpec).compose().asDetLTS()
+    val safety = LTSACall.compile(pSpec).compose().asDetLTS()
+
+    return SupervisoryRobustifier(
+      sys, sys.inputAlphabet,
+      env, env.inputAlphabet,
+      safety, safety.inputAlphabet,
+      progress = listOf("fire_xray", "fire_ebeam"),
+      preferredMap = mapOf(Priority.P3 to listOf(back1, back2), Priority.P2 to listOf(back3)),
+      controllableMap = mapOf(
+        Priority.P1 to listOf("fire_xray", "fire_ebeam"),
+        Priority.P2 to listOf("setMode"),
+        Priority.P3 to listOf("x", "e", "enter", "up", "b")
+      ),
+      observableMap = mapOf(
+        Priority.P0 to listOf("x", "e", "enter", "up", "b", "fire_xray", "fire_ebeam"),
+        Priority.P1 to listOf("setMode")
+      ),
+      synthesizer = SupremicaRunner(),
+      maxIter = 1
+    )
+  }
+
+  private fun loadPump(): SupervisoryRobustifier {
+    val powerSpec =
+      ClassLoader.getSystemResource("specs/pump/power.lts")?.readText() ?: error("Cannot find pump/power.lts")
+    val linesSpec =
+      ClassLoader.getSystemResource("specs/pump/lines.lts")?.readText() ?: error("Cannot find pump/lines.lts")
+    val alarmSpec =
+      ClassLoader.getSystemResource("specs/pump/alarm.lts")?.readText() ?: error("Cannot find pump/alarm.lts")
+    val envSepc =
+      ClassLoader.getSystemResource("specs/pump/deviation.lts")?.readText() ?: error("Cannot find pump/deviation.lts")
+    val pSpec =
+      ClassLoader.getSystemResource("specs/pump/p.lts")?.readText() ?: error("Cannot find pump/p.lts")
+
+    val power = LTSACall.compile(powerSpec).compose().asDetLTS()
+    val lines = LTSACall.compile(linesSpec).compose().asDetLTS()
+    val alarm = LTSACall.compile(alarmSpec).compose().asDetLTS()
+    var sys = cmu.isr.lts.parallelComposition(power, power.inputAlphabet, lines, lines.inputAlphabet)
+    sys = cmu.isr.lts.parallelComposition(sys, sys.inputAlphabet, alarm, alarm.inputAlphabet)
+    val env = LTSACall.compile(envSepc).compose().asDetLTS()
+    val safety = LTSACall.compile(pSpec).compose().asDetLTS()
+
+    val ideal = Word.fromSymbols("plug_in", "battery_charge", "battery_charge", "turn_on", "line.1.dispense_main_med_flow", "line.1.flow_complete")
+    val recover = Word.fromSymbols("plug_in", "line.1.start_dispense", "line.1.dispense_main_med_flow", "line.1.dispense_main_med_flow",
+      "power_failure", "plug_in", "line.1.start_dispense", "line.1.dispense_main_med_flow")
+
+    return SupervisoryRobustifier(
+      sys, sys.inputAlphabet,
+      env, env.inputAlphabet,
+      safety, safety.inputAlphabet,
+      progress = listOf("line.1.flow_complete"),
+      preferredMap = mapOf(Priority.P3 to listOf(ideal, recover)),
+      controllableMap = mapOf(
+        Priority.P0 to listOf(
+          "line.1.start_dispense",
+          "line.1.dispense_main_med_flow",
+          "line.1.flow_complete",
+        ),
+        Priority.P1 to listOf(
+          "line.1.change_settings",
+          "line.1.clear_rate",
+          "line.1.confirm_settings",
+          "line.1.set_rate",
+        ),
+        Priority.P3 to listOf(
+          "line.1.erase_and_unlock_line",
+          "line.1.lock_line",
+          "line.1.lock_unit",
+          "line.1.unlock_unit",
+        )
+      ),
+      observableMap = mapOf(
+        Priority.P0 to listOf(
+          "line.1.start_dispense",
+          "line.1.dispense_main_med_flow",
+          "line.1.flow_complete",
+          "line.1.change_settings",
+          "line.1.clear_rate",
+          "line.1.confirm_settings",
+          "line.1.set_rate",
+        ),
+        Priority.P2 to listOf(
+          // Events of the line that are related to the physical world have moderate cost to observe
+          "line.1.erase_and_unlock_line",
+          "line.1.lock_line",
+          "line.1.lock_unit",
+          "line.1.unlock_unit",
+          // Has moderate cost to observe some system events in other modules
+          "alarm_silence",
+          "enable_alarm",
+        ),
+        Priority.P3 to listOf(
+          // Has high cost to observe some events of other modules related to the physical world.
+          "battery_charge",
+          "battery_spent",
+          "plug_in",
+          "unplug",
+          "turn_off",
+          "turn_on",
+        )
       ),
       synthesizer = SupremicaRunner(),
       maxIter = 1
@@ -135,4 +250,17 @@ class SupervisoryRobustifierTest {
     robustifier.synthesize(Algorithms.Fast).toList()
   }
 
+  @Test
+  fun testTherac() {
+    val robustifier = loadTherac()
+    robustifier.synthesize(Algorithms.Pareto).toList()
+    robustifier.synthesize(Algorithms.Fast).toList()
+  }
+
+  @Test
+  fun testPump() {
+    val robustifier = loadPump()
+    robustifier.synthesize(Algorithms.Pareto).toList()
+    robustifier.synthesize(Algorithms.Fast).toList()
+  }
 }
