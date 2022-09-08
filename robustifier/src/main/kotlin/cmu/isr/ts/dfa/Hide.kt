a6ad0bf17779597c4254cdfe4524492222f91da7
Changjian Zhang <mljzcj@gmail.com>
2022-09-08 13:08:04 -0400

    Refactor NFA/DFA hide

--- robustifier/src/main/kotlin/cmu/isr/ts/dfa/Hide.kt
+++ robustifier/src/main/kotlin/cmu/isr/ts/dfa/Hide.kt
@@ -1,6 +1,6 @@
 package cmu.isr.ts.dfa
 
-import cmu.isr.utils.forEachSetBit
+import cmu.isr.ts.nfa.tauElimination
 import net.automatalib.automata.fsa.DFA
 import net.automatalib.automata.fsa.impl.compact.CompactDFA
 import net.automatalib.words.Alphabet
@@ -8,90 +8,11 @@ import net.automatalib.words.impl.Alphabets
 import java.util.*
 
 fun <S, I> reachableSet(dfa: DFA<S, I>, hidden: Collection<I>): Map<Int, BitSet> {
-  val reachable = mutableMapOf<Int, BitSet>()
-  val stateIDs = dfa.stateIDs()
-  for (state in dfa) {
-    val id = stateIDs.getStateId(state)
-    reachable[id] = BitSet()
-    for (uo in hidden) {
-      val succ = dfa.getSuccessor(state, uo)
-      if (succ != null) {
-        reachable[id]!!.set(stateIDs.getStateId(succ))
-      }
-    }
-  }
-
-  var fixpoint = false
-  while (!fixpoint) {
-    fixpoint = true
-    for (bs in reachable.values) {
-      val oldSize = bs.cardinality()
-      var i = bs.nextSetBit(0)
-      while (i >= 0) {
-        if (i == Int.MAX_VALUE)
-          break
-        bs.or(reachable[i]!!)
-        i = bs.nextSetBit(i+1)
-      }
-      fixpoint = fixpoint && bs.cardinality() == oldSize
-    }
-  }
-
-  return reachable
+  return cmu.isr.ts.nfa.reachableSet(dfa, hidden)
 }
 
 
 fun <S, I> hide(dfa: DFA<S, I>, inputs: Alphabet<I>, hidden: Collection<I>): CompactDFA<I> {
   val observable = inputs - hidden.toSet()
-  val out = CompactDFA(Alphabets.fromCollection(observable))
-  val outStateMap = mutableMapOf<BitSet, Int>()
-  val reachable = reachableSet(dfa, hidden)
-  val stateIDs = dfa.stateIDs()
-  val stack = ArrayDeque<BitSet>()
-
-  // create initial bitset
-  val initStateId = stateIDs.getStateId(dfa.initialState)
-  val initBs = BitSet()
-  initBs.set(initStateId)
-  initBs.or(reachable[initStateId]!!)
-
-  // create output initial state
-  var initAccept = true
-  initBs.forEachSetBit { initAccept = initAccept && dfa.isAccepting(stateIDs.getState(it)) }
-  val initOut = out.addInitialState(initAccept)
-
-  outStateMap[initBs] = initOut
-
-  stack.push(initBs)
-
-  while (stack.isNotEmpty()) {
-    val currBs = stack.pop()
-
-    for (a in observable) {
-      val succBs = BitSet()
-
-      currBs.forEachSetBit {
-        val succState = dfa.getSuccessor(stateIDs.getState(it), a)
-        if (succState != null) {
-          val succStateId = stateIDs.getStateId(succState)
-          succBs.set(succStateId)
-          succBs.or(reachable[succStateId]!!)
-        }
-      }
-
-      if (!succBs.isEmpty) {
-        var outSucc = outStateMap[succBs]
-        if (outSucc == null) {
-          var outSuccAccept = true
-          succBs.forEachSetBit { outSuccAccept = outSuccAccept && dfa.isAccepting(stateIDs.getState(it)) }
-          outSucc = out.addState(outSuccAccept)
-          outStateMap[succBs] = outSucc
-          stack.push(succBs)
-        }
-        out.addTransition(outStateMap[currBs], a, outSucc, null)
-      }
-    }
-  }
-
-  return out
+  return tauElimination(dfa, hidden, observable, CompactDFA(Alphabets.fromCollection(observable))) as CompactDFA<I>
 }
---
+++ robustifier/src/main/kotlin/cmu/isr/ts/nfa/Hide.kt
@@ -0,0 +1,98 @@
+package cmu.isr.ts.nfa
+
+import cmu.isr.utils.forEachSetBit
+import net.automatalib.automata.fsa.MutableFSA
+import net.automatalib.automata.fsa.NFA
+import net.automatalib.automata.fsa.impl.compact.CompactNFA
+import net.automatalib.words.Alphabet
+import net.automatalib.words.impl.Alphabets
+import java.util.*
+
+fun <S, I> reachableSet(nfa: NFA<S, I>, hidden: Collection<I>): Map<Int, BitSet> {
+  val reachable = mutableMapOf<Int, BitSet>()
+  val stateIDs = nfa.stateIDs()
+  for (state in nfa) {
+    val id = stateIDs.getStateId(state)
+    reachable[id] = BitSet()
+    for (uo in hidden) {
+      for (succ in nfa.getSuccessors(state, uo))
+        reachable[id]!!.set(stateIDs.getStateId(succ))
+    }
+  }
+
+  var fixpoint = false
+  while (!fixpoint) {
+    fixpoint = true
+    for (bs in reachable.values) {
+      val oldSize = bs.cardinality()
+      var i = bs.nextSetBit(0)
+      while (i >= 0) {
+        if (i == Int.MAX_VALUE)
+          break
+        bs.or(reachable[i]!!)
+        i = bs.nextSetBit(i+1)
+      }
+      fixpoint = fixpoint && bs.cardinality() == oldSize
+    }
+  }
+
+  return reachable
+}
+
+fun <S, I> hide(nfa: NFA<S, I>, inputs: Alphabet<I>, hidden: Collection<I>): CompactNFA<I> {
+  val observable = inputs - hidden.toSet()
+  return tauElimination(nfa, hidden, observable, CompactNFA(Alphabets.fromCollection(observable))) as CompactNFA<I>
+}
+
+fun <S, I> tauElimination(nfa: NFA<S, I>, hidden: Collection<I>, observable: Collection<I>,
+                          out: MutableFSA<Int, I>): MutableFSA<Int, I> {
+  val outStateMap = mutableMapOf<BitSet, Int>()
+  val reachable = reachableSet(nfa, hidden)
+  val stateIDs = nfa.stateIDs()
+  val stack = ArrayDeque<BitSet>()
+
+  // create initial bitset
+  val initBs = BitSet()
+  for (s in nfa.initialStates) {
+    initBs.set(stateIDs.getStateId(s))
+    initBs.or(reachable[stateIDs.getStateId(s)]!!)
+  }
+
+  // create output initial state
+  var initAccept = true
+  initBs.forEachSetBit { initAccept = initAccept && nfa.isAccepting(stateIDs.getState(it)) }
+  val initOut = out.addInitialState(initAccept)
+
+  outStateMap[initBs] = initOut
+  stack.push(initBs)
+
+  while (stack.isNotEmpty()) {
+    val currBs = stack.pop()
+
+    for (a in observable) {
+      val succBs = BitSet()
+
+      currBs.forEachSetBit {
+        for (succState in nfa.getSuccessors(stateIDs.getState(it), a)) {
+          val succStateId = stateIDs.getStateId(succState)
+          succBs.set(succStateId)
+          succBs.or(reachable[succStateId]!!)
+        }
+      }
+
+      if (!succBs.isEmpty) {
+        var outSucc = outStateMap[succBs]
+        if (outSucc == null) {
+          var outSuccAccept = true
+          succBs.forEachSetBit { outSuccAccept = outSuccAccept && nfa.isAccepting(stateIDs.getState(it)) }
+          outSucc = out.addState(outSuccAccept)
+          outStateMap[succBs] = outSucc
+          stack.push(succBs)
+        }
+        out.addTransition(outStateMap[currBs], a, outSucc, null)
+      }
+    }
+  }
+
+  return out
+}
