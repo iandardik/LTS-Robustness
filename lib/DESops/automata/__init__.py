8636e548f11c318d4b2aaf6d179fde1be0672d15
Ian Dardik <idardik@GS22421.SP.CS.CMU.EDU>
2023-04-26 13:28:05 -0400

    Adding README. Deleting lib/ folder.

--- lib/DESops/automata/__init__.py
+++
@@ -1,3 +0,0 @@
-from DESops.automata.DFA import DFA
-from DESops.automata.NFA import NFA
-from DESops.automata.PFA import PFA
--- lib/DESops/random_automata/__init__.py
+++
@@ -1,2 +0,0 @@
-from ..random_automata.generate import generate
-from ..random_automata.generate_regal import generate_regal
