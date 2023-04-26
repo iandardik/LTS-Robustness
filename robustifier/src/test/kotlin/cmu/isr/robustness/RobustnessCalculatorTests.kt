cbcaf712b7a28de4c166966b94d5f890fc4319d5
Changjian Zhang <mljzcj@gmail.com>
2023-04-25 18:47:24 -0700

    Add disables when generating weakest assumption.

--- robustifier/src/test/kotlin/cmu/isr/robustness/RobustnessCalculatorTests.kt
+++ robustifier/src/test/kotlin/cmu/isr/robustness/RobustnessCalculatorTests.kt
@@ -80,12 +80,12 @@ class RobustnessCalculatorTests {
     assertEquals(expected = expected, actual = actual)
 
     val expectedExplain = setOf(
-      Word.fromSymbols("input", "send.0", "rec.0", "output", "ack.1", "ack.corrupt", "getack.0"),
-      Word.fromSymbols("input", "send.0", "rec.0", "output", "ack.0", "ack.corrupt", "getack.1"),
-      Word.fromSymbols("input", "send.1", "rec.1", "output", "ack.0", "ack.corrupt", "getack.1"),
-      Word.fromSymbols("input", "send.1", "rec.1", "output", "ack.1", "ack.corrupt", "getack.0"),
-      Word.fromSymbols("input", "send.0", "trans.corrupt", "rec.1"),
-      Word.fromSymbols("input", "send.1", "trans.corrupt", "rec.0")
+      Word.fromSymbols("send.0", "rec.0", "ack.1", "ack.corrupt", "getack.0"),
+      Word.fromSymbols("send.0", "rec.0", "ack.0", "ack.corrupt", "getack.1"),
+      Word.fromSymbols("send.1", "rec.1", "ack.0", "ack.corrupt", "getack.1"),
+      Word.fromSymbols("send.1", "rec.1", "ack.1", "ack.corrupt", "getack.0"),
+      Word.fromSymbols("send.0", "trans.corrupt", "rec.1"),
+      Word.fromSymbols("send.1", "trans.corrupt", "rec.0")
     )
     assertEquals(expectedExplain, actual.map { explain.generate(it, cal.weakestAssumption.alphabet()) }.toSet())
   }
@@ -111,20 +111,20 @@ class RobustnessCalculatorTests {
     assertEquals(expected = expected, actual = actual)
 
     val expectedExplain = setOf(
-      Word.fromSymbols("input", "send.0", "rec.0", "output", "ack.1", "ack.corrupt", "getack.0"),
-      Word.fromSymbols("input", "send.0", "rec.0", "output", "ack.0", "ack.corrupt", "getack.1"),
-      Word.fromSymbols("input", "send.1", "rec.1", "output", "ack.0", "ack.corrupt", "getack.1"),
-      Word.fromSymbols("input", "send.1", "rec.1", "output", "ack.1", "ack.corrupt", "getack.0"),
+      Word.fromSymbols("send.0", "rec.0", "ack.1", "ack.corrupt", "getack.0"),
+      Word.fromSymbols("send.0", "rec.0", "ack.0", "ack.corrupt", "getack.1"),
+      Word.fromSymbols("send.1", "rec.1", "ack.0", "ack.corrupt", "getack.1"),
+      Word.fromSymbols("send.1", "rec.1", "ack.1", "ack.corrupt", "getack.0"),
 
-      Word.fromSymbols("input", "send.0", "trans.corrupt", "rec.1", "output", "ack.1", "ack.corrupt", "getack.0"),
-      Word.fromSymbols("input", "send.0", "trans.corrupt", "rec.1", "output", "ack.0", "getack.0"),
-      Word.fromSymbols("input", "send.0", "trans.corrupt", "rec.1", "output", "ack.0", "ack.corrupt", "getack.1"),
-      Word.fromSymbols("input", "send.0", "trans.corrupt", "rec.1", "output", "ack.1", "getack.1"),
+      Word.fromSymbols("send.0", "trans.corrupt", "rec.1", "ack.1", "ack.corrupt", "getack.0"),
+      Word.fromSymbols("send.0", "trans.corrupt", "rec.1", "ack.0", "getack.0"),
+      Word.fromSymbols("send.0", "trans.corrupt", "rec.1", "ack.0", "ack.corrupt", "getack.1"),
+      Word.fromSymbols("send.0", "trans.corrupt", "rec.1", "ack.1", "getack.1"),
 
-      Word.fromSymbols("input", "send.1", "trans.corrupt", "rec.0", "output", "ack.1", "ack.corrupt", "getack.0"),
-      Word.fromSymbols("input", "send.1", "trans.corrupt", "rec.0", "output", "ack.0", "getack.0"),
-      Word.fromSymbols("input", "send.1", "trans.corrupt", "rec.0", "output", "ack.0", "ack.corrupt", "getack.1"),
-      Word.fromSymbols("input", "send.1", "trans.corrupt", "rec.0", "output", "ack.1", "getack.1"),
+      Word.fromSymbols("send.1", "trans.corrupt", "rec.0", "ack.1", "ack.corrupt", "getack.0"),
+      Word.fromSymbols("send.1", "trans.corrupt", "rec.0", "ack.0", "getack.0"),
+      Word.fromSymbols("send.1", "trans.corrupt", "rec.0", "ack.0", "ack.corrupt", "getack.1"),
+      Word.fromSymbols("send.1", "trans.corrupt", "rec.0", "ack.1", "getack.1"),
     )
     for (t in actual) {
       println(explain.generate(t, cal.weakestAssumption.alphabet()))
